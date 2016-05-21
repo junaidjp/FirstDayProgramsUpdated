12032e69846fb1b7b32bda7be92fbc62fef0a9af
junaidjp <junaidpasha@Junaid-Pashas-MacBook-Pro.local>
2016-05-21 12:28:45 -0500

    FirstDay

---
+++ src/MoreMethods.java
@@ -0,0 +1,35 @@
+//More than one method - Program 2 
+public class MoreMethods {
+	
+	public static void main(String args[])
+	{
+		
+		//Create a object of this class;
+		MoreMethods anthony = new MoreMethods();
+		
+		anthony.printDegree();
+		System.out.println("I am Junaid ");
+		printhobbies();
+		printFavouriteFood();
+		
+	}
+	
+	public  void printDegree()
+	{
+		System.out.println("Computer science");
+		
+	}
+	
+	public static void printhobbies()
+	{
+		System.out.println("Java , Cricket ,Long drives ");
+	}
+
+	
+	public static void printFavouriteFood()
+	{
+		System.out.println("Injera,Sushi");
+	
+		
+	}
+}
---
+++ src/ProgrammingStructure.java
@@ -0,0 +1,11 @@
+
+public class ProgrammingStructure {
+	
+	
+	public static void main(String args[])
+	{
+		String name="junaid";
+		System.out.println("My name is "+name);
+	}
+
+}
